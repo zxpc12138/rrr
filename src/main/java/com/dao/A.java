39478560c242590cb155ecaddb8ca3bfb3ef2dbb
zxpc12138 <550037499@qq.com>
2021-11-24 16:18:59 +0800

    ssss

--- src/main/java/com/dao/A.java
+++ src/main/java/com/dao/A.java
@@ -3,10 +3,6 @@ package com.dao;
 public class A {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("4444444446666666666rtr");
-=======
-		System.out.println("26545465456");
->>>>>>> branch 'master' of https://github.com/zxpc12138/rrr.git
+		System.out.println("4444444446666666666rtsdfsr");
 	}
 }
